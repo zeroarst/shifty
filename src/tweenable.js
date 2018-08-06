bd8e3e3c6e966ebd42d8469f6b5b88be75a391f8
Jeremy Kahn <jeremyckahn@gmail.com>
2018-08-06 07:41:19 -0500

    Fix seek test

--- src/tweenable.js
+++ src/tweenable.js
@@ -111,6 +111,47 @@ export const composeEasingObject = (
   return composedEasing;
 };
 
+const processTween = (tween, now) => {
+  const { _currentState, _delay } = tween;
+  let { _duration, _step, _targetState, _timestamp } = tween;
+
+  const endTime = _timestamp + _delay + _duration;
+  let currentTime = Math.min(now, endTime);
+  const hasEnded = currentTime >= endTime;
+  const offset = _duration - (endTime - currentTime);
+
+  if (hasEnded) {
+    _step(_targetState, tween._attachment, offset);
+    tween.stop(true);
+  } else {
+    tween._applyFilter('beforeTween');
+
+    // If the animation has not yet reached the start point (e.g., there was
+    // delay that has not yet completed), just interpolate the starting
+    // position of the tween.
+    if (currentTime < _timestamp + _delay) {
+      currentTime = 1;
+      _duration = 1;
+      _timestamp = 1;
+    } else {
+      _timestamp += _delay;
+    }
+
+    tweenProps(
+      currentTime,
+      _currentState,
+      tween._originalState,
+      _targetState,
+      _duration,
+      _timestamp,
+      tween._easing
+    );
+
+    tween._applyFilter('afterTween');
+    _step(_currentState, tween._attachment, offset);
+  }
+};
+
 /* eslint-disable no-unused-vars */
 export const processQueue = () => {
   const now = Tweenable.now();
@@ -122,44 +163,7 @@ export const processQueue = () => {
       continue;
     }
 
-    const { _currentState, _delay } = tween;
-    let { _duration, _step, _targetState, _timestamp } = tween;
-
-    const endTime = _timestamp + _delay + _duration;
-    let currentTime = Math.min(now, endTime);
-    const hasEnded = currentTime >= endTime;
-    const offset = _duration - (endTime - currentTime);
-
-    if (hasEnded) {
-      _step(_targetState, tween._attachment, offset);
-      tween.stop(true);
-    } else {
-      tween._applyFilter('beforeTween');
-
-      // If the animation has not yet reached the start point (e.g., there was
-      // delay that has not yet completed), just interpolate the starting
-      // position of the tween.
-      if (currentTime < _timestamp + _delay) {
-        currentTime = 1;
-        _duration = 1;
-        _timestamp = 1;
-      } else {
-        _timestamp += _delay;
-      }
-
-      tweenProps(
-        currentTime,
-        _currentState,
-        tween._originalState,
-        _targetState,
-        _duration,
-        _timestamp,
-        tween._easing
-      );
-
-      tween._applyFilter('afterTween');
-      _step(_currentState, tween._attachment, offset);
-    }
+    processTween(tween, now);
   }
 };
 /* eslint-enable no-unused-vars */
@@ -374,11 +378,9 @@ export class Tweenable {
       this._isTweening = true;
       this._isPaused = false;
 
-      // FIXME: This breaks queueing/batching. Switch to shifting the _timestamp back.
-      //
       // If the animation is not running, call timeoutHandler to make sure that
       // any step handlers are run.
-      // this.timeoutHandler(currentTime);
+      processTween(this, currentTime);
 
       this.pause();
     }
